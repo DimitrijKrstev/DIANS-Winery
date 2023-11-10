a715cbe850265449d15b34e0f3b571983df0b3ae
Highperial <antishadowss@gmail.com>
2023-11-10 13:22:15 +0100

    Initial commit

---
+++ src/main/java/macedonia/winery/mkwine/DiansWineryApplication.java
@@ -0,0 +1,13 @@
+package macedonia.winery.mkwine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiansWineryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiansWineryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/macedonia/winery/mkwine/DiansWineryApplicationTests.java
@@ -0,0 +1,13 @@
+package macedonia.winery.mkwine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiansWineryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
