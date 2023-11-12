7712d27cde92aeaa7ced7ab3ddee8d87d60c528c
DimitrijKrstev <antishadowss@gmail.com>
2023-11-12 17:17:06 +0100

    Code cleanup

--- src/main/java/macedonia/winery/mkwine/DiansWineryApplication.java
+++ src/main/java/macedonia/winery/mkwine/DiansWineryApplication.java
@@ -8,11 +8,6 @@ import macedonia.winery.mkwine.pipe.Pipe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.FileWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
