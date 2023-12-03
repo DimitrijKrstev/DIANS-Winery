25b6fdf4d0a54a9067ce5eeadc4cd636a4a8f123
DimitrijKrstev <antishadowss@gmail.com>
2023-12-03 02:35:08 +0100

    updated logo

--- Domashna2/frontend/mk-wine/app/login/page.tsx
+++ Domashna2/frontend/mk-wine/app/login/page.tsx
@@ -6,7 +6,7 @@ export default function Login() {
 		<div className="flex justify-center">
 			<div className="flex flex-col w-[70%] h-max mt-20 bg-gray-800 bg-opacity-50 p-4 rounded-3xl grid-cols-2 gap-10">
 				<div className="flex justify-center">
-					<Image src={Logo} alt="mkwinelogo" className="  w-[45%]  " />
+					<Image src={Logo} alt="mkwinelogo" className="w-[40%]" />
 					<div className="flex flex-col font-body text-5xl mt-16">
 						<label className="mb-3">Username</label>
 						<input type="text" id="name" className="input-primary" name="name" />
