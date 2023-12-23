2bdf38b2d499727cbc737c366574cba49125e93d
DimitrijKrstev <antishadowss@gmail.com>
2023-12-23 16:48:38 +0100

    controller init

--- Domashna3/src/main/java/macedonia/winery/mkwine/model/Comment.java
+++ Domashna3/src/main/java/macedonia/winery/mkwine/model/Comment.java
@@ -2,6 +2,7 @@ package macedonia.winery.mkwine.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
---
+++ Domashna3/src/main/java/macedonia/winery/mkwine/model/Dtos/WineryIdDto.java
@@ -0,0 +1,10 @@
+package macedonia.winery.mkwine.model.Dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class WineryIdDto {
+    Long wineId;
+}
