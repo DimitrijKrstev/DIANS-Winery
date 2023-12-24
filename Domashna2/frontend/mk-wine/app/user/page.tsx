14d26bd2ce7f791885ae8930d3e8402edccbb202
Kristijan Panchevski <kpancevski19@gmail.com>
2023-12-24 20:52:40 +0100

    user changes

--- Domashna2/frontend/mk-wine/app/user/page.tsx
+++ Domashna2/frontend/mk-wine/app/user/page.tsx
@@ -3,10 +3,13 @@ import Logo from "../../public/images/Icon.png";
 import { useEffect, useState, useContext } from "react";
 import Image from "next/image";
 import { GlobalContext } from "../context/page";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = require("next/router");
   const { user, setUser } = useContext(GlobalContext);
   console.log(user.likedWineries);
+  console.log(user);
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="flex flex-col w-full  lg:scale-75   sm:scale-50 lg:h-[65vh] md:h-[65vh] sm:h-[65vh] mt-12 bg-black bg-opacity-50 p-4 lg:rounded-3xl md:rounded-3xl grid-cols-2 gap-5 z-10 ">
@@ -26,14 +29,27 @@ export default function Home() {
             </div>
             <div className=" justify-normal m-[2.5vh] p-[1vh]">
               <label className="mt-3">Vashite vinarii:</label>
-              {user.likedWineries.map((winery: String, index: number) => (
+              {user.likedWineries.map((winery, index: number) => (
                 <div key={index} className=" text-3xl">
-                  {index + 1}. {winery}
+                  {index + 1}. {winery.name}{" "}
+                  <button className="text-clip w-max h-1/4 text-slate-50 text-xl text-center font-Rubik bg-red-600 bg-opacity-70 rounded-[25px]  p-2  mt-8 mr-[4vh]">
+                    Remove
+                  </button>
                 </div>
               ))}
             </div>
           </div>
+          <button
+            onClick={() => {
+              setUser({ email: "", password: "" });
+              window.location.href = "http://localhost:3000";
+            }}
+            className="text-clip w-max text-slate-50 text-xl text-center font-Rubik bg-red-600 rounded-[25px]  p-2  mt-8 "
+          >
+            Checkout
+          </button>
         </div>
+
         <div className="text-center font-body text-2xl"></div>
       </div>
     </div>
