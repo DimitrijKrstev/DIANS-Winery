96316c2335e28b7adf18af53d49d2e58fc884e33
DimitrijKrstev <antishadowss@gmail.com>
2023-12-24 22:39:11 +0100

    fix user crash

--- Domashna2/frontend/mk-wine/app/util/util.tsx
+++ Domashna2/frontend/mk-wine/app/util/util.tsx
@@ -1,74 +1,74 @@
 export function LoginUser(email, password, setUser, setError) {
-  try {
-    fetch("http://localhost:8080/user/login", {
-      method: "POST",
-      body: JSON.stringify({
-        username: email,
-        password: password,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => {
-        console.log(response);
-        if (!response.ok) throw new Error("Invalid username or password");
-        return response.json();
-      })
-      .then((data) => {
-        setUser(data);
-        setError();
-      })
-      .catch((err) => {
-        setError(err);
-      });
-  } catch (err) {
-    setError(err);
-  }
+	try {
+		fetch("http://localhost:8080/user/login", {
+			method: "POST",
+			body: JSON.stringify({
+				username: email,
+				password: password,
+			}),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8",
+			},
+		})
+			.then((response) => {
+				console.log(response);
+				if (!response.ok) throw new Error("Invalid username or password");
+				return response.json();
+			})
+			.then((data) => {
+				setUser(data);
+				setError();
+			})
+			.catch((err) => {
+				setError(err);
+			});
+	} catch (err) {
+		setError(err);
+	}
 }
 
 export function DislikeWine(email, wine, setUser) {
-  try {
-    fetch("http://localhost:8080/user/dislikeWinery", {
-      method: "POST",
-      body: JSON.stringify({
-        userId: email,
-        wineryId: wine,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => {
-        console.log(response);
-        console.log("CAAAAAAAAPPPPPSTEST");
-        if (!response.ok) throw new Error("Invalid username or password");
-        return response.json();
-      })
-      .then((data) => {
-        setUser(data);
-      })
-      .catch((err) => {});
-  } catch (err) {}
+	try {
+		fetch("http://localhost:8080/user/dislikeWinery", {
+			method: "POST",
+			body: JSON.stringify({
+				userId: email,
+				wineryId: wine,
+			}),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8",
+			},
+		})
+			.then((response) => {
+				console.log(response);
+				console.log("CAAAAAAAAPPPPPSTEST");
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+				setUser(data);
+			})
+			.catch((err) => {});
+	} catch (err) {}
 }
 
 export const RegisterUser = (username, email, password, setUser, setError) => {
-  fetch("http://localhost:8080/user/register", {
-    method: "POST",
-    body: JSON.stringify({
-      username: username,
-      email: email,
-      password: password,
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      setUser(data);
-    })
-    .catch((err) => {
-      setError(err);
-    });
+	fetch("http://localhost:8080/user/register", {
+		method: "POST",
+		body: JSON.stringify({
+			username: username,
+			email: email,
+			password: password,
+		}),
+		headers: {
+			"Content-type": "application/json; charset=UTF-8",
+		},
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			setUser(data);
+		})
+		.catch((err) => {
+			setError(err);
+		});
 };
