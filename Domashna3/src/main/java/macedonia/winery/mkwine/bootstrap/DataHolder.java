f826e383b6df646aec2190b47226835b85e3696a
petar <petar.kalinovski@gmail.com>
2023-12-24 01:51:30 +0100

    cleanup of dataholder class

--- Domashna3/src/main/java/macedonia/winery/mkwine/bootstrap/DataHolder.java
+++ Domashna3/src/main/java/macedonia/winery/mkwine/bootstrap/DataHolder.java
@@ -27,35 +27,16 @@ public class DataHolder {
     }
     @PostConstruct
     public void init() throws IOException {
-//        wineryRepository.save(new Winery("Попова Кула",22.2261, 41.410614,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Винарија Грков",22.128117, 41.315422,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Domaine Lepovo",22.034212, 41.517864,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Камник",21.490385, 42.007866,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Vinarija Stobi",21.936052, 41.581486,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Винарската визба КАРТАЛ",21.476423, 41.962433,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Семејна винарија „Захарчев“",22.004782, 41.43615,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Шато Сопот",21.7409905, 41.777774,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Бовин винарија",22.0915153, 41.4943704,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Тиквеш",21.4394921, 41.9940075,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Kamnik",21.4272901, 41.9950477,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Winery Temov",21.4358622, 42.0001818,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Сковин",21.4310531, 41.9956025,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("NemaIme",22.1940439, 41.737897,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Stobi Winery",21.4209535, 42.0000999,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("Château Sopot Winery",21.740898, 41.7774687,new ArrayList<Comment>(),new ArrayList<Wine>()));
-//        wineryRepository.save(new Winery("monastery winery",20.877133, 41.1848848,new ArrayList<Comment>(),new ArrayList<Wine>()));
         ObjectMapper objectMapper = new ObjectMapper();
         File file=new File("Domashna1/output.json");
         try {
             Map<String, Object> resultMap = objectMapper.readValue(file, new TypeReference<Map<String, Object>>() {});
 
-            // Get the content under the "features" key
             Object features = resultMap.get("features");
 
             if (features instanceof List) {
                 List<Object> featuresList = (List<Object>) features;
 
-                // Iterate through the list of features
                 for (Object feature : featuresList) {
                     Map<String, Object> featureMap = (Map<String, Object>) feature;
                     Map<String, Object> properties = (Map<String, Object>) featureMap.get("properties");
@@ -66,7 +47,6 @@ public class DataHolder {
                     String email = null;
                     String amenity = null;
                     if (properties != null) {
-                        System.out.println("Properties:");
 
                         int k=0;
                         for (Map.Entry<String, Object> entry : properties.entrySet()) {
